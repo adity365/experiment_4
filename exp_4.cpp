8f88e2d493315277d6e6790023717c72d945d954
adity365 <aditya.agarwal.20.08.2002@gmail.com>
2024-09-02 09:18:33 +0530

    Add files via upload

---
+++ exp_4.cpp
@@ -0,0 +1,20 @@
+// Aditya Agarwal
+// 23070123162
+# include <iostream>
+using namespace std;
+
+int main(){
+    int a = 3 ;
+    int b = 4;
+    // Bitwise operators
+    cout << "a & b is: " << (a & b) << endl;
+    cout << "a | b is: " << (a | b) << endl;
+    cout << "~a is: " << (~a) << endl;
+    cout << "a ^ b is: " << (a ^ b) << endl;
+    
+    // left shift and right shift 
+    cout << (17>>1) << endl;
+    cout <<(17 >> 2) << endl;
+    cout << (19 << 1) << endl;
+    cout << (21 << 2) << endl;
+}
